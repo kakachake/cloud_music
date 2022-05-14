841c70daa5f21b76b956daac4d28d8c146496d4c
kakachake <282281884@qq.com>
2022-05-14 11:19:59 +0800

    feat: fixed

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,7 +1,9 @@
 // const BASE_URL = 'http://localhost:3000/api/v1'
 //通过环境变量来切换环境
 const BASE_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:4000' : 'http://localhost:4000'
+  process.env.NODE_ENV === 'development'
+    ? 'https://netease-cloud-music-api-rho-liart.vercel.app/'
+    : 'https://netease-cloud-music-api-rho-liart.vercel.app/'
 
 const TIME_OUT = 5000
 
