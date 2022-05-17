877c28fc75fe0bbecac6d62d0d7969b715c0b484
kakachake <282281884@qq.com>
2022-05-17 10:46:03 +0800

    fix: bug fix

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -3,7 +3,7 @@
 const BASE_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://101.43.155.53:9001/'
-    : 'http://101.43.155.53:9001/'
+    : 'https://netease-cloud-music-api-rho-liart.vercel.app/'
 
 const TIME_OUT = 5000
 
